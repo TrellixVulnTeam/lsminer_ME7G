fe1f7322ef34ae8395b1847a87fdc4c460fea362
wtxpwh <wtxpwh@gmail.com>
2019-08-25 22:00:40 +0800

    update client.py. add socket process

--- client.py
+++ client.py
@@ -6,9 +6,13 @@ import uuid
 import json
 import os
 import logging
+import queue
+import threading
 
 logging.basicConfig(level = logging.INFO,format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
+
+q = queue.Queue(0)
 
 class lsminerClient(object):
     def __init__(self):
@@ -39,7 +43,9 @@ class lsminerClient(object):
         reqjson = json.dumps(reqData)
         logging.info(reqjson)
         self.sock = socket.create_connection((self.cfg['ip'], self.cfg['port']), 3)
-        self.sock.sendall(reqjson)
+        self.sock.setblocking(True)
+        self.sock.settimeout(None)
+        self.sock.sendall(reqjson.encode("utf-8"))
 
 
     def loadCfg(self):
@@ -49,16 +55,94 @@ class lsminerClient(object):
         if 'ip' not in self.cfg or 'port' not in self.cfg:
             raise ValueError('config file error. missing ip or port!')
 
-while True:
-    try:
-        client = lsminerClient()
-        client.loadCfg()
+    def processMsg(self, msg):
+        if 'method' in msg:
+            if msg['method'] == 1:
+                pass
+            elif msg['method'] == 2:
+                pass
+            elif msg['method'] == 3:
+                pass
+            elif msg['method'] == 4:
+                pass
+            elif msg['method'] == 5:
+                pass
+            elif msg['method'] == 6:
+                pass
+            elif msg['method'] == 7:
+                pass
+            elif msg['method'] == 8:
+                pass
+            elif msg['method'] == 9:
+                pass
+            elif msg['method'] == 10:
+                pass
+            elif msg['method'] ==11:
+                pass
+            elif msg['method'] == 12:
+                pass
+            elif msg['method'] == 13:
+                pass
+            elif msg['method'] == 14:
+                pass
+            else:
+                print('unknown server msg method! msg: ' + str(msg))
+        else:
+            print('unknown server msg! msg: ' + str(msg))
 
-    except Exception as e:
-        print("main loop run exception. msg: " + str(e))
-        print("sleep 3 seconds and retry.")
-        time.sleep(3)
-    
+
+    def recvThread(self):
+        buffer = []
+        while True:
+            if self.sock == None:
+                print('client socket == None. sleep one second.')
+                time.sleep(1)
+                continue
+
+            rd = self.sock.recv(4096).decode()
+            if rd:
+                buffer.append(rd)
+                data = ''.join(buffer)
+                try:
+                    msg = json.loads(data)
+                    self.processMsg(msg)
+                    buffer.clear()
+                except Exception as e:
+                    print('recv thread exception. msg: ' + str(e))
+                    print('json.loads server data exception.')
+            else:
+                print('server close socket. exit recv thread.')
+                self.sock.close()
+                break
+
+    def processCmd(self, cmd):
+        pass
+
+    def init(self):
+        self.loadCfg()
+        #self.loginSrv()
+        thread = threading.Thread(target=lsminerClient.recvThread, args=(self,))
+        thread.start()
+
+    def run(self):
+        q.put(1)
+        while True:
+            try:
+                cmd = q.get()
+                self.processCmd(cmd)
+            except Exception as e:
+                print("main loop run exception. msg: " + str(e))
+                print("sleep 3 seconds and retry...")
+                time.sleep(3)
+
+
+if __name__ == '__main__':
+    client = lsminerClient()
+    client.init()
+    client.run()
+
+
+
     
 
 
