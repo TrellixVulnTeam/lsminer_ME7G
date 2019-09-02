349d1db7d1a151b89ab61d6eaa9412733fd74967
wtxpwh <wtxpwh@163.com>
2019-09-02 12:31:01 +0800

    update gpumon.py

--- gpumon.py
+++ gpumon.py
@@ -36,20 +36,21 @@ def nvmlGetGpuInfo():
     if nvHandle:
         count = ffi.new("int*", 0)
         lib.wrap_nvml_get_gpucount(nvHandle, count)
-        deviceinfo = {}
-        for i in range(count):
+        for i in range(count[0]):
+            deviceinfo = {}
             name = ffi.new("char[128]")
-            tmp = ffi.new("unsigned int*", 0)
-            lib.wrap_nvml_get_gpu_name(nvHandle, i, name)
+            tempC = ffi.new("unsigned int*", 0)
+            fanpcnt = ffi.new("unsigned int*", 0)
+            power_usage = ffi.new("unsigned int*", 0)
+            lib.wrap_nvml_get_gpu_name(nvHandle, i, name, 128)
             deviceinfo['name'] = ffi.string(name).decode()
-            lib.wrap_nvml_get_tempC(nvHandle, i, tmp)
-            deviceinfo['tempC'] = tmp[0]
-            lib.wrap_nvml_get_fanpcnt(nvHandle, i, tmp)
-            deviceinfo['fanpcnt'] = tmp[0]
-            lib.wrap_nvml_get_power_usage(nvHandle, i, tmp)
-            deviceinfo['power_usage'] = tmp[0]
+            lib.wrap_nvml_get_tempC(nvHandle, i, tempC)
+            deviceinfo['tempC'] = tempC[0]
+            lib.wrap_nvml_get_fanpcnt(nvHandle, i, fanpcnt)
+            deviceinfo['fanpcnt'] = fanpcnt[0]
+            lib.wrap_nvml_get_power_usage(nvHandle, i, power_usage)
+            deviceinfo['power_usage'] = power_usage[0]
             info.append(deviceinfo)
-            deviceinfo.clear()
     return info
 
 def amdGetGpuInfo():
@@ -57,11 +58,11 @@ def amdGetGpuInfo():
     if amdHandle:
         count = ffi.new("int*", 0)
         lib.wrap_adl_get_gpucount(amdHandle, count)
-        deviceinfo = {}
-        for i in range(count):
+        for i in range(count[0]):
+            deviceinfo = {}
             name = ffi.new("char[128]")
             tmp = ffi.new("unsigned int*", 0)
-            lib.wrap_adl_get_gpu_name(amdHandle, i, name)
+            lib.wrap_adl_get_gpu_name(amdHandle, i, name, 128)
             deviceinfo['name'] = ffi.string(name).decode()
             lib.wrap_adl_get_tempC(amdHandle, i, tmp)
             deviceinfo['tempC'] = tmp[0]
@@ -70,9 +71,9 @@ def amdGetGpuInfo():
             lib.wrap_adl_get_power_usage(amdHandle, i, tmp)
             deviceinfo['power_usage'] = tmp[0]
             info.append(deviceinfo)
-            deviceinfo.clear()
     return info
 
 if __name__ == '__main__':
     print(amdGetGpuInfo())
     print(nvmlGetGpuInfo())
+    pass
