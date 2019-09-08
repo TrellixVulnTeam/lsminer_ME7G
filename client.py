03f77fb6c4d7108ccdd8009312c91fec2b400c81
wtxpwh <wtxpwh@163.com>
2019-09-08 12:29:22 +0800

    add sysfs

--- client.py
+++ client.py
@@ -1,4 +1,4 @@
-
+#!/usr/bin/python3
 from lsminer import *
 import time
 import socket
--- gpumon.py
+++ gpumon.py
@@ -24,6 +24,23 @@ ffi.cdef('''
     int wrap_adl_get_fanpcnt(wrap_adl_handle* adlh, int gpuindex, unsigned int* fanpcnt);
     int wrap_adl_get_power_usage(wrap_adl_handle* adlh, int gpuindex, unsigned int* milliwatts);
 
+    typedef void wrap_amdsysfs_handle;
+    typedef struct _pciInfo
+    {
+        int DeviceId = -1;
+        int HwMonId = -1;
+        int PciDomain = -1;
+        int PciBus = -1;
+        int PciDevice = -1;
+    } pciInfo;
+    wrap_amdsysfs_handle* wrap_amdsysfs_create();
+    int wrap_amdsysfs_destroy(wrap_amdsysfs_handle* sysfsh);
+    int wrap_amdsysfs_get_gpucount(wrap_amdsysfs_handle* sysfsh, int* gpucount);
+    int wrap_amdsysfs_get_tempC(wrap_amdsysfs_handle* sysfsh, int index, unsigned int* tempC);
+    int wrap_amdsysfs_get_fanpcnt(wrap_amdsysfs_handle* sysfsh, int index, unsigned int* fanpcnt);
+    int wrap_amdsysfs_get_power_usage(wrap_amdsysfs_handle* sysfsh, int index, unsigned int* milliwatts);
+    int wrap_amdsysfs_get_pciInfo(wrap_amdsysfs_handle* sysfsh, int index, pciInfo* info);
+
 ''')
 
 if platform.system() == 'Linux':
