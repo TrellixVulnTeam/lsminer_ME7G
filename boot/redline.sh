433850ed5a6d4c037d75912c10f5af0f276d43e4
wtxpwh <wtxpwh@163.com>
2019-09-09 18:51:44 +0800

    update

---
+++ boot/redline.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python3 /home/lsminer/lsminer/redline.py
---
+++ redline.py
@@ -0,0 +1,24 @@
+import time
+import os
+from lsminer import *
+
+'''循环检测云端指令是否需要重启系统'''
+while True:
+    try:
+        cfg = loadCfg()
+        if cfg == 0:
+            raise ValueError('loadcfg error!')
+        if 'reboot' in cfg:
+            url = cfg['reboot'] + getWkid()
+        else:
+            raise ValueError('config file error!')
+        reboot = getReboot(url)
+        if reboot:
+            os.system("reboot")
+        else:
+            time.sleep(60)
+    except Exception as e:
+        print("main loop run exception. msg: " + str(e))
+        print("sleep 3 seconds and retry.")
+        time.sleep(3)
+
