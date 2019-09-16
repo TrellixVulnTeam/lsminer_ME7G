259879703382fc1bef8eeb29abd5e79d4bcfded3
wtxpwh <wtxpwh@163.com>
2019-09-16 10:34:19 +0800

    update redline

--- redline.py
+++ redline.py
@@ -1,24 +1,23 @@
-import time
-import os
+
 from tools import *
+import logging
+
+logging.basicConfig(level = logging.INFO, format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s', datefmt = '%Y-%m-%d  %H:%M:%S %a')
 
 '''循环检测云端指令是否需要重启系统'''
 while True:
     try:
-        cfg = loadCfg()
-        if 'reboot' in cfg:
-            url = cfg['reboot'] + getWkid()
-        else:
-            raise ValueError('config file error!')
+        url = 'http://api.lsminer.com:37124/redline/'
         reboot = getReboot(url)
         if reboot:
-            print('system will be reboot')
+            logging.warning('recv reboot cmd! system will be reboot.')
             #os.system("sudo reboot")
         else:
-            print('sleep 60 seconds...')
+            print('sleep 60 seconds. and check reboot cmd.')
             time.sleep(60)
     except Exception as e:
-        print("main loop run exception. msg: " + str(e))
-        print("sleep 3 seconds and retry.")
+        logging.error("main loop run exception. msg: " + str(e))
+        logging.error("sleep 3 seconds and retry.")
+        logging.exception(e)
         time.sleep(3)
 
