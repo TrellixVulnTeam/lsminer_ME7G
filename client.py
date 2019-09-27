3766cfe0229ed6b3f23a5c7cb000494bd93a9fd9
wtxpwh <wtxpwh@163.com>
2019-09-27 15:41:49 +0800

    wkname bug fix

--- client.py
+++ client.py
@@ -130,7 +130,7 @@ class lsminerClient(object):
             if self.cfg['wkname']:
                 reqData['wkname'] = self.cfg['wkname']
             else:
-                reqData['wkname'] = getLanIp().replace('.', '-')
+                reqData['wkname'] = getLanIp().replace('.', '_')
 
             if self.cfg['wkid']:
                 reqData['wkid'] = self.cfg['wkid']
