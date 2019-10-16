2be7e2d0ca282a26157dfa59cc7552eb0ca4a54c
Fei <splxsg@gmail.com>
2019-10-16 11:51:59 +0800

    Update update.py

--- update.py
+++ update.py
@@ -48,6 +48,12 @@ def checkClientUpdate(ver, url):
                 if downloadFile(resdict['appurl'], filepath):
                     logging.info(getFileMd5(filepath))
                     logging.info(resdict['appmd5'])
+                    logging.info(type(getFileMd5(filepath)))
+                    logging.info(type(resdict['appmd5']))
+                    if getFileMd5(filepath) == resdict['appmd5']:
+                        logging.info('123123')
+                    else:
+                        logging.info('gg')
                     if getFileMd5(filepath) == resdict['appmd5']:
                         logging.info('file download ok.')
                         stopService()
