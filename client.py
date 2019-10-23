7369b7923bfeabeedb6b353f29cd84237c2626d2
Fei <splxsg@gmail.com>
2019-10-23 15:49:16 +0800

    Update client.py

--- client.py
+++ client.py
@@ -114,7 +114,7 @@ class lsminerClient(object):
             time.sleep(3)
             q.put(1)
     
-    def GenerateAMDdeviceID(gpuinfo)
+    def GenerateAMDdeviceID(self,gpuinfo)
         gpustatus = ""
         try:
             if gpuinfo:
