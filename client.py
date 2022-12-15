39dbd5920fc50d34ee93aca24fd2221c8814b3ed
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-15 07:04:28 +0000

    Adding tarfile member sanitization to extractall()

--- client.py
+++ client.py
@@ -419,7 +419,26 @@ class lsminerClient(object):
                 time.sleep(3)
 
             with tarfile.open(path) as tar:
-                tar.extractall('./miners')
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(tar, "./miners")
                 #os.remove(path)
                 self.minerpath = './miners/' + mcfg['minerver'] + '/' + mcfg['minername']
                 return self.minerpath
